f55298b1f701e98a7aa30991319c03c6819ce96b
Dale Corns <codefellow@gmail.com>
2018-07-29 18:17:53 -0700

    add guid generator

---
+++ generate-guid.js
@@ -0,0 +1,14 @@
+/**
+ * generate-guid
+ * Created by dcorns on 7/29/18
+ * Copyright © 2018 Dale Corns
+ * MIT Licensed
+ */
+'use strict';
+const crypto = require('crypto');
+const config = require('./config');
+module.exports = (email) => {
+  return crypto.createHmac('sha256', config.SECRET)
+    .update(email)
+    .digest('hex');
+};
